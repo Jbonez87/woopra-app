be3c16dfd4821e6e18e3a51f46acef5b0353ab50
John Castrillon <jcastrillon87@yahoo.com>
2017-09-14 12:56:19 -0400

    add makePhotos function for initial page load photos

--- src/App.js
+++ src/App.js
@@ -5,21 +5,55 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      photos: []
-    };
+      photos: [],
+    }
+    this.makePhotos = this.makePhotos.bind(this);
   }
   componentDidMount() {
-
+    fetch('http://localhost:3001/api/photos')
+    .then(res => {
+      return res.json()
+    })
+    .then(photos => {
+      // console.log(photos);
+      this.setState({
+        photos: photos,
+      })
+    })
+  }
+  makePhotos(photos) {
+    Object.keys(photos).map((key) => {
+      const photo = photos[key];
+      return Object.keys(photo).map((idx) => {
+        const photoItem = photo[idx];
+        // console.log(photoItem);
+        return (
+          <div>
+            <h2>{photoItem.title}</h2>
+          </div>
+        );
+      })
+    })
   }
   render() {
     return (
-      <div class="col-lg-6">
-        <h1>Welcome to Woopra search!</h1>
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search for..." />
-            <span class="input-group-btn">
-              <button class="btn btn-secondary" type="button">Go!</button>
-            </span>
+      <div className="container">
+        <div className="page-header">
+          <h1>Welcome to Woopra search!</h1>
+        </div>
+        <div className="row">
+          <div className="col-lg-4 col-centered">
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Search for..." />
+                <span className="input-group-btn">
+                  <button className="btn btn-success" type="button">Go!</button>
+                </span>
+            </div>
+          </div>
+        </div>
+        <div className="black">
+          {this.makePhotos(this.state.photos)}
+          {/* {console.log(this.state.photos)} */}
         </div>
       </div>
     );
